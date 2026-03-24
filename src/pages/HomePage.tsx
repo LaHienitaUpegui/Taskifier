fb7069eda0294133c9f66272b231adacf71607bc
Juan Diego Moreno Upegui <juan42774@gmail.com>
2026-03-24 17:45:10 -0500

    feat: filter HomePage to show only active projects

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,12 +6,16 @@ import Button from "../components/Button";
 function HomePage() {
     const mockProjects: Project[] = projectData as Project[];
 
+    const activeProjects = mockProjects.filter(
+        (project) => project.status === "active",
+    );
+
     return (
         <div className="homepage">
             <h1>Recent projects</h1>
 
             <section className="projects-displayer">
-                {mockProjects.map((project) => (
+                {activeProjects.map((project) => (
                     <div key={project.id} className="project-container">
                         <div className="title-and-desc">
                             <h6 className="project-title">{project.name}</h6>
