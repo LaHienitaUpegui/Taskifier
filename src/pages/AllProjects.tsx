2f8b62816dc5669213dd275c2703c0e835fc086e
Juan Diego Moreno Upegui <juan42774@gmail.com>
2026-03-25 10:35:15 -0500

    feat: add create project button and integrate ProjectInfoModal in AllProjects

--- src/pages/AllProjects.tsx
+++ src/pages/AllProjects.tsx
@@ -3,10 +3,14 @@ import "../styles/all-projects-page.css";
 import type { Project } from "../types";
 import allProjectsData from "../data/projects.json";
 import Button from "../components/Button";
+import ProjectInfoModal from "../components/ProjectInfoModal";
 
 function AllProjects() {
     const mockProjects: Project[] = allProjectsData as Project[];
 
+    const [selectedProject, setSelectedProject] = useState<Project | null>(
+        null,
+    );
     const [statusSelected, setStatusSelected] = useState<string>("active");
     const [searchTerm, setSearchTerm] = useState<string>("");
 
@@ -24,6 +28,10 @@ function AllProjects() {
             return matchesStatus && matchesSearchTerm;
         });
     }, [mockProjects, statusSelected, searchTerm]);
+
+    function handleSetSelectedProject(project: Project | null) {
+        setSelectedProject(project);
+    }
 
     return (
         <div className="all-projects-page">
@@ -58,25 +66,121 @@ function AllProjects() {
 
             <section className="projects-displayer">
                 {filteredProjects.length > 0 ? (
-                    filteredProjects.map((project) => (
-                        <div key={project.id} className="project-container">
-                            <h6 className="project-title">{project.name}</h6>
-                            <div className="buttons">
-                                <Button
-                                    innerText="Open project"
-                                    buttonType="primary"
-                                />
-                                <Button
-                                    innerText="See info"
-                                    buttonType="secundary"
-                                />
+                    <>
+                        {statusSelected === "active" && (
+                            <button className="create-project-button">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="28"
+                                    height="28"
+                                    viewBox="0 0 24 24"
+                                    className="add-icon"
+                                >
+                                    <path
+                                        fill="var(--dark-gray-icons-fill)"
+                                        d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1"
+                                        className="add-icon-path"
+                                    />
+                                </svg>
+                                Create Project
+                            </button>
+                        )}
+
+                        {filteredProjects.map((project) => (
+                            <div key={project.id} className="project-container">
+                                <h6 className="project-title">
+                                    {project.name}
+                                </h6>
+                                <div className="buttons">
+                                    <Button
+                                        innerText="Open project"
+                                        buttonType="primary"
+                                    />
+                                    <Button
+                                        innerText="See info"
+                                        buttonType="secundary"
+                                        onClickFunction={() =>
+                                            handleSetSelectedProject(project)
+                                        }
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    ))
+                        ))}
+                    </>
                 ) : (
-                    <p className="no-projects-message">No projects found.</p>
+                    <div className="no-projects-found">
+                        <p>No projects found</p>
+                        <small>Check if the name is correct</small>
+                    </div>
                 )}
             </section>
+
+            <ProjectInfoModal
+                isOpen={!!selectedProject}
+                onClose={() => handleSetSelectedProject(null)}
+            >
+                {selectedProject && (
+                    <div className="selected-project-info">
+                        <div className="title-and-desc">
+                            <h3>{selectedProject.name}</h3>
+                            <small>{selectedProject.description}</small>
+                        </div>
+
+                        <div className="tasks-info-container">
+                            <p className="tasks-subtitle">
+                                Tasks of the project
+                            </p>
+
+                            <div className="tasks-information">
+                                <div className="info-container">
+                                    <small className="info-subtitle">
+                                        Completed
+                                    </small>
+                                    <small>
+                                        {
+                                            selectedProject.tasks.filter(
+                                                (task) =>
+                                                    task.status === "completed",
+                                            ).length
+                                        }
+                                    </small>
+                                </div>
+
+                                <div className="info-container">
+                                    <small className="info-subtitle">
+                                        Pending
+                                    </small>
+                                    <small>
+                                        {
+                                            selectedProject.tasks.filter(
+                                                (task) =>
+                                                    task.status === "pending",
+                                            ).length
+                                        }
+                                    </small>
+                                </div>
+
+                                <div className="info-container">
+                                    <small className="info-subtitle">
+                                        Total
+                                    </small>
+                                    <small>
+                                        {selectedProject.tasks.length}
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Button
+                            innerText="Open project"
+                            buttonType="primary"
+                            onClickFunction={() =>
+                                handleSetSelectedProject(null)
+                            }
+                        />
+                    </div>
+                )}
+            </ProjectInfoModal>
         </div>
     );
 }
